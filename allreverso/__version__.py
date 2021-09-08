198760e5aede800ba1fce5013852e32a55265223
Fire <70655051+PetitPotiron@users.noreply.github.com>
2021-09-08 17:32:51 +0200

    Create __version__.py

---
+++ allreverso/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'allreverso'
+__description__ = 'A simple package to handle allreverso.net services (translation, voice, dictionary etc.).'
+__url__ = 'https:/github.com/PetitPotiron/allreverso/'
+__version__ = '1.2.7'
+__build__ = 0x1
+__author__ = '🔥 Fire'
+__author_discord__ = '🔥 Fire#8388 (id : 715826047949471785)'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c) 2021 🔥 Fire'
