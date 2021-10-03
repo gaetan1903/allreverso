c830582aa52792cf78f1f31d6ac1620b8ccd2dee
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2021-10-03 11:23:55 +0200

    add test unitaire & Integration Continue

---
+++ tests/test.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.dirname('/'.join(__file__.split('/')[:-1])))
+import allreverso
+
+client = allreverso.ReversoClient()
+
+class TestCaseModule(unittest.TestCase):
+    def test_translate(self):
+        '''
+            Test du fonctionnalité de traduction
+        '''
+        res = client.translate("fr", "en", "Bonjour !")
+        ''' ce fonction retourne un liste, pour anticiper une changement
+        on verifie que c'est bien un donnée de type `list` '''
+        self.assertTrue(isinstance(res, list))
+        # On verifie qu'il y a bien une reponse dans la liste.
+        self.assertTrue(res[0])
+    
+    def test_correct(self):
+        '''
+            Test du fonctionnalité de traduction
+        '''
+        res = client.correct("fr", "Bonjoure !").text
+        # verification que le resultat existe
+        self.assertTrue(res)
+    
+    def test_synonymize(self):
+        '''
+            Test du fonctionnalité de traduction
+        '''
+        res = client.synonymize("fr", "bateau")
+        # verification que le resultat existe
+        self.assertTrue(res)
+
